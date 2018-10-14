909200d9b984289d01a18cb5de2ae1daef1f6895
Jan Brockmeyer <jan.brockmeyer@mgm-tp.com>
2018-10-14 15:17:41 +0200

    Example with tesseract.js in browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,140 @@
 import * as React from "react";
-import "./App.css";
+import Dropzone, { FileWithPreview } from "react-dropzone";
+import { Grid, Header, Segment, Progress } from "semantic-ui-react";
+import { TesseractStatic } from "tesseract.js";
 
-import logo from "./logo.svg";
+const style = {
+  h1: {
+    marginTop: "3em"
+  },
+  h2: {
+    margin: "4em 0em 2em"
+  },
+  h3: {
+    marginTop: "2em",
+    padding: "2em 0em"
+  },
+  last: {
+    marginBottom: "300px"
+  }
+};
 
-class App extends React.Component {
+const { Column } = Grid;
+
+const Tesseract: TesseractStatic = (window as any).Tesseract;
+
+interface StateType {
+  result: string | undefined;
+  fileName: string | undefined;
+  progress: number | undefined;
+  status: string | undefined;
+  imageSrc: string | undefined;
+}
+
+class App extends React.Component<object, StateType> {
+  public initialState = {
+    result: undefined,
+    progress: undefined,
+    fileName: undefined,
+    status: undefined,
+    imageSrc: undefined
+  };
+
+  public state = this.initialState;
+
   public render() {
+    const { progress, status, result } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <Header
+          as="h1"
+          content="OCR Demo"
+          style={style.h1}
+          textAlign="center"
+        />
+        {progress !== undefined &&
+          status !== undefined && (
+            <Progress
+              percent={Math.floor(progress * 100)}
+              indicating={progress < 1 && result === undefined}
+              success={progress === 1 && result !== undefined}
+              label={status}
+            />
+          )}
+        <Grid columns={3} stackable={true}>
+          <Column>
+            <Header
+              as="h3"
+              content="File Upload"
+              style={style.h3}
+              textAlign="center"
+            />
+            <Dropzone onDrop={this.handleDrop} onClick={this.handleClick} />
+          </Column>
+          <Column>
+            {this.state.imageSrc !== undefined && (
+              <>
+                <Header
+                  as="h3"
+                  content={`Original Image: ${this.state.fileName}`}
+                  style={style.h3}
+                  textAlign="center"
+                />
+                <img
+                  src={this.state.imageSrc}
+                  style={{ width: "auto", maxHeight: "300px" }}
+                />
+              </>
+            )}
+          </Column>
+          <Column>
+            {this.state.result !== undefined && (
+              <>
+                <Header
+                  as="h3"
+                  content="Recognized Text"
+                  style={style.h3}
+                  textAlign="center"
+                />
+                <Segment>{this.state.result}</Segment>
+              </>
+            )}
+          </Column>
+        </Grid>
       </div>
     );
   }
+
+  private handleDrop = (
+    accepted: FileWithPreview[],
+    rejected: FileWithPreview[],
+    event: React.DragEvent<HTMLDivElement>
+  ) => {
+    accepted.forEach(file => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        if (typeof reader.result === "string") {
+          this.setState({ imageSrc: reader.result });
+        }
+      };
+      reader.readAsDataURL(file);
+      this.setState({ fileName: file.name });
+
+      Tesseract.recognize(file, { lang: "deu" })
+        .progress(({ progress, status }) => {
+          this.setState({ progress, status });
+        })
+        .catch(err => console.error(err))
+        .then(result => {
+          this.setState({ result: result.text });
+        })
+        .finally(resultOrError => console.log(resultOrError));
+    });
+  };
+
+  private handleClick = () => {
+    this.setState(this.initialState);
+  };
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
+import "semantic-ui-css/semantic.min.css";
+
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+
 import App from "./App";
-import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
